a6d90cd8a992ecc68ca59de9fe0d8ee36c797559
hegedus103 <hegedus103@freemail.hu>
2019-07-24 11:46:04 +0200

    MarhaelhullasController create

---
+++ app/Http/Controllers/Marha/MarhaelhullasController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Marha;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MarhaelhullasController extends Controller
+{
+    //
+}
