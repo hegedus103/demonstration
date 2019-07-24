289661c5925bef6ff5a17fd8b1bfcf8898a48b8b
hegedus103 <hegedus103@freemail.hu>
2019-07-24 11:46:52 +0200

    MarhakikerulesController create

---
+++ app/Http/Controllers/Marha/MarhakikerulesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Marha;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MarhakikerulesController extends Controller
+{
+    //
+}
