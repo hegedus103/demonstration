0fc1f3e3fa44a6a1fd32df53cc61897134ffe4be
hegedus103 <hegedus103@freemail.hu>
2019-07-24 12:00:11 +0200

    A kernel.php change

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        
     ];
 
     /**
@@ -35,6 +36,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\GenerateMenus::class,
+          
         ],
 
         'api' => [
@@ -60,6 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+         'Marhadisplay' => \App\Http\Middleware\Marha\MarhadisplayMiddleware::class,
+         'Marhakikerules' => \App\Http\Middleware\Marha\MarhakikerulesMiddleware::class,
     ];
 
     /**
