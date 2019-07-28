c5f2627000e85c8221491f5613a5a0ae3ecbdb78
hegedus103 <hegedus103@freemail.hu>
2019-07-28 11:42:51 +0200

    marhaadatok_modositasa_create.blade create

---
+++ resources/views/sajat/madatok_modositasa/marha_sajat_vagas_modositas_main.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+@include('sajat.madatok_modositasa.marha_sajat_vagas_modositas_create')
+@endsection
+
+@section('fomenu')
+@include('sajat.munkasztalfomenu')
+@endsection
+
+@section('hatter')
+@include('sajat.marhaadatok.bevitelhatter')
+@endsection
