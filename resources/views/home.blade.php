961ee7bb57a275fd2fdd61b116e334415256365b
hegedus103 <hegedus103@freemail.hu>
2019-07-28 11:16:23 +0200

    home.blade and welcome.blade create

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('fomenu')
+@include('sajat.munkasztalfomenu')
+@endsection
+
+
+@section('content')
+@include('sajat.menu_marha.menu_marha')
+
+@endsection
+
+@section('hatter')
+@include('sajat.asztalhatter')
+@endsection
