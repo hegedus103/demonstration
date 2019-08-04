ebeb2d74cf41462722cc3f0770c6269ee48dbb4c
hegedus103 <hegedus103@freemail.hu>
2019-08-04 10:37:05 +0200

    verify.blade.php create

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+
+
+@section('fomenu')
+@include ('sajat.fomenu')
+@endsection
+
+@section('content')
+<div class="container">
+                  <div class="foimage">
+     {{ Html::image('img/margit.jpg', 'a picture') }}
+        </div>
+    <div class="row justify-content-center  login-content-align">
+        <div class="col-md-8">
+            <div class="login-top-pos">
+                <div class="login-contener-fejlec">{{ __('Regisztráció') }}</div>
+                <div class="card-body login-contener-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Neve')}}</label>
+
+                            <div class="col-md-6">
+
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                @if ($errors->has('name'))
+                                   <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Azonosító') }}</label>
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required>
+                                @if ($errors->has('username'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('username') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="tenyeszetkod" class="col-md-4 col-form-label text-md-right">{{ __('Tenyészetkód') }}</label>
+                            <div class="col-md-6">
+                                <input id="tenyeszetkod" type="text" class="form-control{{ $errors->has('tenyeszetkod') ? ' is-invalid' : '' }}" name="tenyeszetkod" value="{{ old('tenyeszetkod') }}" required>
+                                @if ($errors->has('tenyeszetkod'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('tenyeszetkod') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail címe') }}</label>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
