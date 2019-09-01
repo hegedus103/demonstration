531f932a6a5810df2b0474adf8d30e99589257da
hegedus103 <hegedus103@freemail.hu>
2019-09-01 09:50:48 +0200

    menu.marha create

---
+++ resources/js/components/menu/marhatorzs_menu.vue
@@ -0,0 +1,60 @@
+<template>
+  <sidebar-menu :menu="menu" />
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                menu: [
+                    {
+                        header: true,
+                        title: 'Main Navigation',
+                        // component: componentName
+                        // visibleOnCollapse: true
+                        // class:''
+                        // attributes: {}
+                    },
+                    { // item
+                        href: '/',
+                        title: 'Dashboard',
+                        icon: 'fa fa-user',
+                        /*
+                        // custom icon
+                        icon: {
+                            element: 'span',
+                            class: 'fa fa-user',
+                            attributes: {}
+                        }
+                        */
+                        // disabled: true
+                        // class: ''
+                        // attributes: {}
+                        // alias: '/path'
+                        /*
+                        badge: {
+                            text: 'new',
+                            class: 'default-badge'
+                            // attributes: {}
+                            // element: 'span'
+                        }
+                        */
+                    },
+                    { // item with child
+                        href: '/charts',
+                        title: 'Charts',
+                        icon: 'fa fa-chart-area',
+                        child: [
+                            {
+                                href: '/charts/sublink',
+                                title: 'Sub Link',
+                            }
+                        ]
+                    },
+                    {  },
+                    {  }
+                ]
+            }
+        }
+    }
+</script>
